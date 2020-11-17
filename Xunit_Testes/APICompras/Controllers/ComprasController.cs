4d0a08291adc43956e130b6a15acb3ac31600676
yuriDuque <yurithielmann83@gmail.com>
2020-11-17 18:58:15 -0200

    enviando projeto apiCompras

---
+++ Xunit_Testes/APICompras/Controllers/ComprasController.cs
@@ -0,0 +1,59 @@
+﻿using APICompras.Models;
+using APICompras.Services;
+using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
+
+namespace APICompras.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ComprasController : ControllerBase
+    {
+        private readonly ICompraService _service;
+        public ComprasController(ICompraService service)
+        {
+            _service = service;
+        }
+        // GET api/compras
+        [HttpGet]
+        public ActionResult<IEnumerable<CompraItem>> Get()
+        {
+            var items = _service.GetAllItems();
+            return Ok(items);
+        }
+        // GET api/compras/5
+        [HttpGet("{id}")]
+        public ActionResult<CompraItem> Get(int id)
+        {
+            var item = _service.GetById(id);
+            if (item == null)
+            {
+                return NotFound();
+            }
+            return Ok(item);
+        }
+        // POST api/compras
+        [HttpPost]
+        public ActionResult Post([FromBody] CompraItem value)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+            var item = _service.Add(value);
+            return CreatedAtAction("Get", new { id = item.Id }, item);
+        }
+        // DELETE api/compras/5
+        [HttpDelete("{id}")]
+        public ActionResult Remove(int id)
+        {
+            var existingItem = _service.GetById(id);
+            if (existingItem == null)
+            {
+                return NotFound();
+            }
+            _service.Remove(id);
+            return Ok();
+        }
+    }
+}
---
+++ Xunit_Testes/APICompras/Models/CompraItem.cs
@@ -0,0 +1,13 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace APICompras.Models
+{
+    public class CompraItem
+    {
+        public int Id { get; set; }
+        [Required]
+        public string Nome { get; set; }
+        public decimal Preco { get; set; }
+        public string Fabricante { get; set; }
+    }
+}
