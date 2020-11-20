51b934814736dca7b672d40c8b6d152771bcf552
Yuri Duque <yurithielmann83@gmail.com>
2020-11-20 11:38:07 -0300

    retornando com projeto

---
+++ Xunit_Testes/APICompras/Services/CompraService.cs
@@ -0,0 +1,26 @@
+﻿using APICompras.Models;
+using System;
+using System.Collections.Generic;
+
+namespace APICompras.Services
+{
+    public class CompraService : ICompraService
+    {
+        public CompraItem Add(CompraItem novoItem)
+        {
+            throw new NotImplementedException();
+        }
+        public IEnumerable<CompraItem> GetAllItems()
+        {
+            throw new NotImplementedException();
+        }
+        public CompraItem GetById(int id)
+        {
+            throw new NotImplementedException();
+        }
+        public void Remove(int id)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ Xunit_Testes/APICompras/Services/ICompraService.cs
@@ -0,0 +1,13 @@
+﻿using APICompras.Models;
+using System.Collections.Generic;
+
+namespace APICompras.Services
+{
+    public interface ICompraService
+    {
+        IEnumerable<CompraItem> GetAllItems();
+        CompraItem Add(CompraItem novoItem);
+        CompraItem GetById(int id);
+        void Remove(int id);
+    }
+}
